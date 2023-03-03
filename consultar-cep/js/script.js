05f075a7f4582d5bb87c8725f98a9f4e4ad3c162
guilherme nunes <gnlnascimento@gmail.com>
2023-03-03 00:14:06 -0300

    Edição das páginas para se tornarem mais organizadas e responsivas

--- consultar-cep/js/script.js
+++ consultar-cep/js/script.js
@@ -1,68 +1,68 @@
 const pesquisarCep = async () => {
-  let cep = document.getElementById('cep').value;
+	let cep = document.getElementById('cep').value;
 
-  if (cep.trim() === '') {
-    document.getElementById(
-      'result'
-    ).innerHTML = `<h5 class='warning'>O campo Buscar CEP deve ser preenchido!</h5>`;
-  } else {
-    await fetch(`https://viacep.com.br/ws/${cep}/json/`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((address) => {
-        if (!address.erro) {
-          document.getElementById(
-            'result'
-          ).innerHTML = `<div class="card" style="width: 18rem;">
+	if (cep.trim() === '') {
+		document.getElementById(
+			'result'
+		).innerHTML = `<h5 class='warning'>O campo Buscar CEP deve ser preenchido!</h5>`;
+	} else {
+		await fetch(`https://viacep.com.br/ws/${cep}/json/`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`HTTP error: ${response.status}`);
+				}
+				return response.json();
+			})
+			.then(address => {
+				if (!address.erro) {
+					document.getElementById(
+						'result'
+					).innerHTML = `<div class="card" style="width: 18rem;">
 					<div class="card-body">
 					<h5 class="card-title">Endereço da Clínica</h5>
 					</div>
 					<ul class="list-group list-group-flush">
 					<li class="list-group-item"><b>Bairro:</b> ${address.bairro}</li>
               		<li class="list-group-item"><b>Logradouro:</b> ${
-                    address.logradouro
-                  }</li>
+										address.logradouro
+									}</li>
                <li class="list-group-item"><b>Bairro:</b> ${address.bairro}</li>
            ${
-             address.complemento
-               ? `<li class='list-group-item'>
+							address.complemento
+								? `<li class='list-group-item'>
                  <b>Complemento:</b> ${address.complemento}
                       </li>`
-               : ''
-           }
+								: ''
+						}
             <li class="list-group-item"><b>Localidade:</b> ${
-              address.localidade
-            }</li>
+							address.localidade
+						}</li>
                  <li class="list-group-item"><b>UF:</b> ${address.uf}</li>
               </ul>
             </div>`;
-        } else {
-          document.getElementById(
-            'result'
-          ).innerHTML = `<h5>Erro! CEP não encontrado</h5>`;
-        }
-      })
-      .catch((error) => {
-        alert(`
+				} else {
+					document.getElementById(
+						'result'
+					).innerHTML = `<h5>Erro! CEP não encontrado</h5>`;
+				}
+			})
+			.catch(error => {
+				alert(`
         Erro! Tente novamente!
         ${error.status}
         ${error.message}`);
-      });
-  }
+			});
+	}
 };
 
 const clearInput = () => {
-  document.getElementById('cep').value = '';
-  document.getElementById('result').innerHTML = '';
+	document.getElementById('cep').value = '';
+	document.getElementById('result').innerHTML = '';
 };
 
 // ----------------------- Usuários -----------------------
@@ -72,41 +72,41 @@ const listaDeUsuario = document.querySelector('.listaDeUsuario');
 let pessoas = [];
 
 btnListPeople.addEventListener('click', () => {
-  pessoas = [];
-  listaDeUsuario.innerHTML = '';
-  // Container da imagem
-  let imageCont = document.createElement('div');
-  let img = document.createElement('img');
-  img.classList.add('personImage');
-  let userImage = document.querySelector('.personImage');
-  imageCont.appendChild(img);
+	pessoas = [];
+	listaDeUsuario.innerHTML = '';
+	// Container da imagem
+	let imageCont = document.createElement('div');
+	let img = document.createElement('img');
+	img.classList.add('personImage');
+	let userImage = document.querySelector('.personImage');
+	imageCont.appendChild(img);
 
-  fetch('https://randomuser.me/api/?results=10')
-    .then((response) => {
-      if (response.ok) return response.json();
-      else {
-        throw new Error('Não conseguimos ter acesso à imagem');
-      }
-    })
-    .then((data) => {
-      data.results.map((item) => {
-        pessoas.push({
-          nome: item.name,
-          idade: item.dob.age,
-          image: item.picture.medium,
-        });
-      });
-      pessoas.forEach((pessoa) => {
-        console.log(pessoa);
-        listaDeUsuario.innerHTML += `<li class ='liStyle'>
+	fetch('https://randomuser.me/api/?results=10')
+		.then(response => {
+			if (response.ok) return response.json();
+			else {
+				throw new Error('Não conseguimos ter acesso à imagem');
+			}
+		})
+		.then(data => {
+			data.results.map(item => {
+				pessoas.push({
+					nome: item.name,
+					idade: item.dob.age,
+					image: item.picture.medium,
+				});
+			});
+			pessoas.forEach(pessoa => {
+				console.log(pessoa);
+				listaDeUsuario.innerHTML += `<li class ='liStyle'>
         <p>  Nome: ${pessoa.nome.first} ${pessoa.nome.last} Idade: ${pessoa.idade} </p>
         <img src = '${pessoa.image}'} alt ='foto'/>
         </li>`;
-      });
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
+			});
+		})
+		.catch(error => {
+			alert(error.message);
+		});
 });
 
 // actions form
@@ -115,64 +115,64 @@ let dados = [];
 let ultimoIndiceExibido = 0;
 
 const submit = document.getElementById('enviar');
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
+submit.addEventListener('click', e => {
+	e.preventDefault();
 
-  let name = document.getElementById('nome').value;
-  let age = document.getElementById('idade').value;
-  let email = document.getElementById('email').value;
-  let nameMedic = document.getElementById('nomeDoMedico').value;
-  let comment = document.getElementById('comment').value;
+	let name = document.getElementById('nome').value;
+	let age = document.getElementById('idade').value;
+	let email = document.getElementById('email').value;
+	let nameMedic = document.getElementById('nomeDoMedico').value;
+	let comment = document.getElementById('comment').value;
 
-  if (
-    name === '' ||
-    age === '' ||
-    email === '' ||
-    nameMedic === '' ||
-    comment === ''
-  ) {
-    alert('algum campo precisa ser preenchido!');
-  } else {
-    dados.push({
-      name: name,
-      age: age,
-      email: email,
-      nameMedic: nameMedic,
-      comment: comment,
-    });
-    console.log(dados);
-    chamarComentarios();
-    document.getElementById('nome').value = '';
-    document.getElementById('idade').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('nomeDoMedico').value = '';
-    document.getElementById('comment').value = '';
-  }
+	if (
+		name === '' ||
+		age === '' ||
+		email === '' ||
+		nameMedic === '' ||
+		comment === ''
+	) {
+		alert('algum campo precisa ser preenchido!');
+	} else {
+		dados.push({
+			name: name,
+			age: age,
+			email: email,
+			nameMedic: nameMedic,
+			comment: comment,
+		});
+		console.log(dados);
+		chamarComentarios();
+		document.getElementById('nome').value = '';
+		document.getElementById('idade').value = '';
+		document.getElementById('email').value = '';
+		document.getElementById('nomeDoMedico').value = '';
+		document.getElementById('comment').value = '';
+	}
 });
 
 const date = new Date();
 let day = date.getDay();
 let year = date.getFullYear();
 const mesesAno = [
-  'JAN',
-  'FEV',
-  'MAR',
-  'ABR',
-  'MAI',
-  'JUN',
-  'JUL',
-  'AGO',
-  'SET',
-  'OUT',
-  'NOV',
-  'DEZ',
+	'JAN',
+	'FEV',
+	'MAR',
+	'ABR',
+	'MAI',
+	'JUN',
+	'JUL',
+	'AGO',
+	'SET',
+	'OUT',
+	'NOV',
+	'DEZ',
 ];
 let month = mesesAno[date.getMonth()];
 
 const chamarComentarios = () => {
-  for (let i = ultimoIndiceExibido; i < dados.length; i++) {
-    let div = document.createElement('div');
-    div.innerHTML = `<div class="card" style="width: 18rem;">                
+	for (let i = ultimoIndiceExibido; i < dados.length; i++) {
+		let div = document.createElement('div');
+		div.innerHTML = `<div class="card" style="width: 18rem;">                
 		<div class="card-body">
       <ion-icon class="icon-profile" name="person-circle-outline" ></ion-icon>
       <div>
@@ -193,7 +193,7 @@ const chamarComentarios = () => {
     </div>              
 		</div>
 		</div> <hr/>`;
-    comments.appendChild(div);
-  }
-  ultimoIndiceExibido = dados.length;
+		comments.appendChild(div);
+	}
+	ultimoIndiceExibido = dados.length;
 };
--- login/js/script.js
+++ login/js/script.js
@@ -1,23 +1,31 @@
 let user = [
-  {
-    cpf: '12345678910',
-    senha: 'coxinha123',
-  },
+	{
+		cpf: '12345678910',
+		senha: 'coxinha123',
+	},
+	{
+		cpf: '35796657891',
+		senha: 'vemSerFeliz',
+	},
+	{
+		cpf: '19034765816',
+		senha: 'amoJs',
+	},
 ];
 
 const padraoCpf = /^[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}$/;
 
 const loginButton = document.querySelector('.login-btn');
 
-loginButton.addEventListener('click', (el) => {
-  let cpf = document.getElementById('cpf').value;
-  let password = document.getElementById('password').value;
-  el.preventDefault();
-  if (cpf === '' || password === '' || !padraoCpf.test(cpf)) {
-    alert('Algum dos campos está inválido, preencha-os corretamente');
-  } else if (cpf == '12345678910' && password == 'coxinha123') {
-    window.location.href = '../logado/index.html';
-  } else {
-    alert('Cliente não encontrado, verifique os dados');
-  }
+loginButton.addEventListener('click', el => {
+	let cpf = document.getElementById('cpf').value;
+	let password = document.getElementById('password').value;
+	el.preventDefault();
+	if (cpf === '' || password === '' || !padraoCpf.test(cpf)) {
+		alert('Algum dos campos está inválido, preencha-os corretamente');
+	} else if (cpf == '12345678910' && password == 'coxinha123') {
+		window.location.href = '../logado/index.html';
+	} else {
+		alert('Cliente não encontrado, verifique os dados');
+	}
 });
