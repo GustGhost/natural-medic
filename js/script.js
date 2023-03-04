a3750478e74c42d6b63546ed52b1ad483ff480c3
guilherme nunes <gnlnascimento@gmail.com>
2023-03-04 13:01:20 -0300

    Criação de uma pasta para o css global e a refatoração do código da página sobre (finalizada, falta responsividade

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 function check() {
-  let storedName = localStorage.getItem('name');
-  console.log(storedName);
+	let storedName = localStorage.getItem('name');
+	console.log(storedName);
 
-  if (storedName) {
-    document.getElementById(
-      'login'
-    ).innerHTML = `<p>Bem vindo</p> <p>${storedName}</p>`;
-  } else {
-    document.getElementById(
-      'login'
-    ).innerHTML = `<button><a href="../login/index.html">Entrar</a></button>`;
-  }
+	if (storedName) {
+		document.getElementById(
+			'login'
+		).innerHTML = `<p>Bem vindo</p> <p>${storedName}</p>`;
+	} else {
+		document.getElementById(
+			'login'
+		).innerHTML = `<button class ='login-btn-green'><a href="../login/index.html">Entrar</a></button>`;
+	}
 }
 
 check();
