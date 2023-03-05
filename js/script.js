d1c97a75c0f1ec50d80d1ca8ce0fd96e6836014a
Pablo henrique <pabloenosteteucpx@gmail.com>
2023-03-05 13:10:37 -0300

    resolvendo error do console log ao chamar o id

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function check() {
 	let storedName = localStorage.getItem('name');
-
+	let titleDashboard = document.getElementById('titleDashboard');
 	if (storedName) {
 		document.getElementById(
 			'login'
 		).innerHTML = `<div style="text-align: center;"><p>Bem vindo</p> <p>${storedName.substr(0, 10)}</p></div>`;
-		document.getElementById('titleDashboard').innerHTML = `<h3>Bem vindo ${storedName}</h3>`;
+		if (titleDashboard) {
+			titleDashboard.innerHTML = `<h3>Bem vindo ${storedName}</h3>`;
+		}
 	} else {
 		document.getElementById(
 			'login'
